abf437b86f0bab06a231d65ea08886775f986bc6
winstoncwang <51161138+winstoncwang@users.noreply.github.com>
2020-05-26 16:09:04 +0100

    1.1
    adding SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
-const App = () => {
-	return <div>Success</div>;
-};
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}
 
 export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+	componentDidMount () {}
+
+	render () {
+		return <div>searchbar</div>;
+	}
+}
+
+export default SearchBar;
