fbf6142abd8ab1e600e5d4c2792758b416d45c53
winstoncwang <51161138+winstoncwang@users.noreply.github.com>
2020-05-27 12:24:14 +0100

    1.2
    adding config searchbar component, eventhandler and form submission

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
 	render () {
 		return (
-			<div>
+			<div className="ui grid container">
 				<SearchBar />
 			</div>
 		);
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 
 class SearchBar extends React.Component {
-	componentDidMount () {}
+	//state
+	state = { term: '' };
+
+	async componentDidMount () {
+		//get method
+		// const searchResult = await axios.get(
+		// 	'https://www.googleapis.com/youtube/v3/search',
+		// 	{
+		// 		header : {},
+		// 		params : {
+		// 			part : this.props.term
+		// 		}
+		// 	}
+		// );
+	}
+
+	onSubmitForm (e) {
+		e.preventDefault();
+	}
 
 	render () {
-		return <div>searchbar</div>;
+		return (
+			<div className="ui segment wide column search-bar">
+				<form className="ui form" onSubmit={this.onSubmitForm}>
+					<div className="field">
+						<label>Video Search:</label>
+						<input
+							type="text"
+							placeholder="Search your favourite Vclip...."
+							onChange={(e) => {
+								this.setState({ term: e.target.value });
+							}}
+							value={this.props.term}
+						/>
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
